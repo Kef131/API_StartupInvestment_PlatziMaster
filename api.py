701e909c9e2142f03589b2a65db54d57bd4765ec
Kef <kevin.gdl.131@gmail.com>
2021-04-19 18:22:39 -0500

    Ugly ugly example of a functional API

--- api.py
+++ api.py
@@ -1,11 +1,38 @@
 import flask
+import pandas
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
+#load dataset with pandas
+# prework - clean informacion - define information ( optional)
+# functions of what we wanna do
+# statistics etc
+# hacer funciones que definan los datos que vamos a mostrar
+
+# Definition of routes in our API
+#
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
+#Get all founders of the dataset
+@app.route('/founders', methods=['GET'])
+def founders():
+    return "<h1>ALL FOUNDERS</h1>";
+
+#Highest investment of the dataset
+# @app.route('/invesment/highest', methods=['GET'])
+# def highestInvestment():
+#     #call the function, output information in json
+#     return objectJson
+
+
+
+# @app.route('/investor/{id}', methods=['GET'])
+# def highestInvestment():
+#
+#     return objectJson
+#
 app.run()
