5f8e9b69c909acc8ec9b437cd0c6c91c55d800ba
Kef <kevin.gdl.131@gmail.com>
2021-04-23 15:56:01 -0500

    Ready to be tested on Digital Ocean

--- app.py
+++ app.py
@@ -4,23 +4,15 @@ import pandas as pd
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
-#load dataset with pandas
-# prework - clean informacion - define information ( optional)
-# functions of what we wanna do
-# statistics etc
-# hacer funciones que definan los datos que vamos a mostrar
-
 # Definition of routes in our API
-#
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Pokemon Dataset</h1><p>This is a example API to test the data set of pokemon</p>"
 
 #Get all founders of the dataset
-@app.route('/statuses', methods=['GET'])
+@app.route('/names', methods=['GET'])
 def statuses():
-    return print(companies['status'].value_counts())
+    return pokemon['Name'].value_counts()
 
 #Highest investment of the dataset
 # @app.route('/invesment/highest', methods=['GET'])
@@ -29,14 +21,10 @@ def statuses():
 #     return objectJson
 
 
-
-
 # @app.route('/investor/{id}', methods=['GET'])
 # def highestInvestment():
-#
 #     return objectJson
-#
 
 if __name__ == "__main__":
-    companies = pd.read_csv('DataSet_StartupInvestment/objects.csv', low_memory=False)
+    pokemon = pd.read_csv('PokeTypeMatchupData.csv', low_memory=False)
     app.run(port=8080)
